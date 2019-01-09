c95676abbaae5c0eed8cd6de8996a87c2e86a718
st3inum <st3inum@gmail.com>
2019-01-09 14:39:55 +0600

    second

--- main/game.cpp
+++ main/game.cpp
@@ -8,7 +8,7 @@ class gari
 {
     Texture text_C,text_G,passenger_body,passenger_matha,bg1,bg2,bg3,text_c;
     Sprite chaka1,chaka2,gari,pass_body,pass_matha,bg_w_1,bg_w_2,bg_w_3;
-    int pos_x=-10,pos_y=386,pos_w_x=-10,d=1280*2+15,wind_f=1280; // d for total distance you want to travel ;)
+    int pos_x=-10,pos_y=386,pos_w_x=-10,d=12800*2+15,wind_f=1280; // d for total distance you want to travel ;)
 public:
     void load_chaka(string txt)
     {
@@ -138,7 +138,7 @@ public:
     }
     void print()
     {
-        window1.clear(Color::White);
+        // window1.clear(Color::White);
         window1.draw(bg_w_1);
         window1.draw(bg_w_2);
         window1.draw(bg_w_3);
@@ -147,13 +147,113 @@ public:
         window1.draw(chaka1);
         window1.draw(chaka2);
         window1.draw(gari);
-        window1.display();
+        // window1.display();
+    }
+};
+
+class pathor
+{
+
+    Texture pat;
+    Sprite pa;
+    int pos_x=-1,pos_y=386,pos_w_x=-10,d=1280*2+15,wind_f=1280; // d for total distance you want to travel ;)
+    double curr_v=10,uthbe=500.0/2;
+    int badha=500,flag=1;
+    public:
+    bool f=0;
+    void load_pathor(string txt,int ori)
+    {
+        // pat.setSmooth(true);
+        pat.loadFromFile(txt);
+        pa.setColor(Color(255,255,255,255));
+        pa.setTexture(pat);
+        pa.setOrigin(pa.getLocalBounds().width/2, pa.getLocalBounds().height/2);
+        pa.setPosition(ori,10);
+        pa.scale(0.09,0.09);
+    }
+    void move()
+    {
+        if(Keyboard::isKeyPressed(Keyboard::Right))
+        {
+           pa.move(-10,0);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Left))
+        {
+            pa.move(10,0);
+        }
+        if(uthbe>10)
+        {
+            if(flag==1)
+            {
+                if(pa.getPosition().y<badha)
+                {
+                    pa.move(0.0,curr_v);
+                    curr_v+=(flag*9.8/30.0);
+                }
+                else 
+                {
+                    flag*=-1;
+                    curr_v/=2;
+                    pa.move(0.0,-curr_v);
+                    curr_v+=(flag*9.8/30.0);
+                }
+            }
+            else
+            {
+                if(pa.getPosition().y>badha-uthbe && curr_v>0)
+                {
+                    pa.move(0.0,-curr_v);
+                    curr_v+=(flag*9.8/30.0);
+                }
+                else 
+                {
+                    uthbe/=3;
+                    flag*=-1;
+                    // curr_v=0;
+                    pa.move(0.0,curr_v);
+                    curr_v+=(flag*9.8/30.0);
+                }
+            }
+        }
+        else
+        {
+            if(pa.getPosition().y<badha)
+            {
+                f=1;
+                pa.move(0.0,curr_v);
+                curr_v+=(flag*9.8/30.0);
+            }
+        }
+        // cout<<flag<<' '<<pa.getPosition().y<<' '<<curr_v<<endl;
+        
+    }
+    void print()
+    {
+        window1.draw(pa);
     }
 };
 
 
+
+
 int main() {
-    window1.setFramerateLimit(30);
+    window1.setFramerateLimit(60);
+
+
+    srand(time(NULL));
+    pathor tmp;
+    int n=6;
+    std::vector<pathor> pat(n);
+    for(int i=0;i<n;i++)
+    {
+        int x=rand()%win_W;
+        pat[i].load_pathor("file/stone.png",x);
+    }
+
+
+
+
+
     gari new_gari;
     new_gari.load_bg("file/bg_3.jpg");
     new_gari.load_chaka("file/car_gravel_tire_ultra_lowres.png");
@@ -165,8 +265,22 @@ int main() {
         while(window1.pollEvent(event)) {
             if(event.type == event.Closed) window1.close();
         }
+        window1.clear(Color::White);
         new_gari.move();
         new_gari.print();
+        for(int i=0;i<pat.size();i++){
+            pat[i].move();
+            if(pat[i].f){
+                pat.erase(pat.begin()+i);
+            }
+        }
+        for(int i=0;i<pat.size();i++){
+            pat[i].print();
+        }
+
+
+
+        window1.display();
     }
     return 0;
 }
