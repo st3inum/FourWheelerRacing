7c81c25e138ba5f6614f55677a248843eb9c27c8
st3inum <st3inum@gmail.com>
2019-01-20 17:50:52 +0600

    Add files via upload

--- main/life.cpp
+++ main/life.cpp
@@ -6,31 +6,76 @@ using namespace std;
 // RenderWindow window1;
 RenderWindow window1(VideoMode(win_W, 620), "Four Wheeler Racing");
 
-/*
+
 class life
 {
-    Texture pat;
-    int pos_x=-1,pos_y=386,pos_w_x=-10,d=1280*2+15,wind_f=1280; // d for total distance you want to travel ;)
-    double curr_v=10,uthbe=500.0/2;
-    int badha=480;
-    int opacity=255,blink_time=0;
+    Texture heart;
     public:
-    Sprite pa[9];
+    Sprite hea[10];
     bool f=0;
     int op_cnt=0;
-    void scale()
+    void init()
     {
-        pa.scale(0.2,0.2);
-    }
-    void load_life(string txt,int ori)
-    {
-        // pat.setSmooth(true);
-        pat.loadFromFile(txt);
-        pa.setColor(Color(255,255,255,255));
-        pa.setTexture(pat);
-        pa.setOrigin(pa.getLocalBounds().width/2, pa.getLocalBounds().height/2);
-        pa.setPosition(ori,10);
-        cout<<"# "<<txt<<endl;
+        int i=0;
+        hea[i].setPosition(500,400);
+        hea[i].setTexture(heart);
+        hea[i].setTextureRect(IntRect(0,0,252,217.5));
+        hea[0].setOrigin(127.5,108);
+        hea[i].scale(0.2,0.2);
+        i++;
+        hea[i].setPosition(500,400);
+        hea[i].setTexture(heart);
+        hea[i].setTextureRect(IntRect(276,0,492-276,217.5));
+        hea[1].setOrigin(396-276,108);
+        hea[i].scale(0.2,0.2);
+        i++;
+        hea[i].setPosition(500,400);
+        hea[i].setTexture(heart);
+        hea[i].setTextureRect(IntRect(516,0,694.5-516,217.5));
+        hea[2].setOrigin(628.5-516,108);
+        hea[i].scale(0.2,0.2);
+        i++;
+        hea[i].setPosition(500,400);
+        hea[i].setTexture(heart);
+        hea[i].setTextureRect(IntRect(696.5,0,836.5-696.5,217.5));
+        hea[3].setOrigin(790.5-696.5,108);
+        hea[i].scale(0.2,0.2);
+        i++;
+        hea[i].setPosition(500,400);
+        hea[i].setTexture(heart);
+        hea[i].setTextureRect(IntRect(862,0,938-862,217.5));
+        hea[4].setOrigin(902-862,108);
+        hea[i].scale(0.2,0.2);
+        i++;
+        hea[i].setPosition(500,400);
+        hea[i].setTexture(heart);
+        hea[i].setTextureRect(IntRect(82.5,217.5,168-82.5,239.5));
+        hea[5].setOrigin(124.5-82.5,101);
+        hea[i].scale(0.2,0.2);
+        i++;
+        hea[i].setPosition(500,400);
+        hea[i].setTexture(heart);
+        hea[i].setTextureRect(IntRect(192,217.5,334.4-192,239.5));
+        hea[6].setOrigin(238.5-192,101);
+        hea[i].scale(0.2,0.2);
+        i++;
+        hea[i].setPosition(500,400);
+        hea[i].setTexture(heart);
+        hea[i].setTextureRect(IntRect(333,217.5,516-333,239.5));
+        hea[7].setOrigin(396-333,101);
+        hea[i].scale(0.2,0.2);
+        i++;
+        hea[i].setPosition(500,400);
+        hea[i].setTexture(heart);
+        hea[i].setTextureRect(IntRect(516,217.5,763.5-516,239.5));
+        hea[8].setOrigin(630-516,101);
+        hea[i].scale(0.2,0.2);
+        i++;
+        hea[i].setPosition(500,400);
+        hea[i].setTexture(heart);
+        hea[i].setTextureRect(IntRect(763.5,217.5,1014-763.5,239.5));
+        hea[9].setOrigin(897-763.5,101);
+        hea[i].scale(0.2,0.2);
     }
     void move()
     {
@@ -74,7 +119,7 @@ class life
     }
 };
 
-*/
+
 
 
 
@@ -93,71 +138,71 @@ int main() {
     hea[i].setTexture(heart);
     hea[i].setTextureRect(IntRect(0,0,252,217.5));
     hea[0].setOrigin(127.5,108);
-    hea[i].scale(0.5,0.5);
+    hea[i].scale(0.2,0.2);
     i++;
     hea[i].setPosition(500,400);
     hea[i].setTexture(heart);
     hea[i].setTextureRect(IntRect(276,0,492-276,217.5));
     hea[1].setOrigin(396-276,108);
-    hea[i].scale(0.5,0.5);
+    hea[i].scale(0.2,0.2);
     i++;
     hea[i].setPosition(500,400);
     hea[i].setTexture(heart);
     hea[i].setTextureRect(IntRect(516,0,694.5-516,217.5));
     hea[2].setOrigin(628.5-516,108);
-    hea[i].scale(0.5,0.5);
+    hea[i].scale(0.2,0.2);
     i++;
     hea[i].setPosition(500,400);
     hea[i].setTexture(heart);
     hea[i].setTextureRect(IntRect(696.5,0,836.5-696.5,217.5));
     hea[3].setOrigin(790.5-696.5,108);
-    hea[i].scale(0.5,0.5);
+    hea[i].scale(0.2,0.2);
     i++;
     // hea[4].setOrigin(937.108);
     hea[i].setPosition(500,400);
     hea[i].setTexture(heart);
     hea[i].setTextureRect(IntRect(862,0,938-862,217.5));
     hea[4].setOrigin(902-862,108);
-    hea[i].scale(0.5,0.5);
+    hea[i].scale(0.2,0.2);
     i++;
 
 
 
 
-    hea[i].setPosition(500-30,400-40);
+    hea[i].setPosition(500,400);
     hea[i].setTexture(heart);
-    hea[i].setTextureRect(IntRect(919.5,501.0,960-919.5,766.5-501.0));
-    hea[i].rotate(180);
-    hea[5].setOrigin(937.5-919.5,627.0-501);
-    hea[i].scale(-0.5,-0.5);
+    hea[i].setTextureRect(IntRect(82.5,217.5,168-82.5,239.5));
+    // hea[i].rotate(180);
+    hea[5].setOrigin(124.5-82.5,101);
+    hea[i].scale(0.2,0.2);
     i++;
-    hea[i].setPosition(500-30,400-40);
+    hea[i].setPosition(500,400);
     hea[i].setTexture(heart);
-    hea[i].setTextureRect(IntRect(706.5,511.5,817.5-706.5,765-511.5));
-    hea[i].rotate(180);
-    hea[6].setOrigin(768-706.5,630-511.5);
-    hea[i].scale(-0.5,-0.5);
+    hea[i].setTextureRect(IntRect(192,217.5,334.4-192,239.5));
+    // hea[i].rotate(180);
+    hea[6].setOrigin(238.5-192,101);
+    hea[i].scale(0.2,0.2);
     i++;
-    hea[i].setPosition(500-30,400-40);
+    hea[i].setPosition(500,400);
     hea[i].setTexture(heart);
-    hea[i].setTextureRect(IntRect(489,510,687-489,765-510));
-    hea[i].rotate(180);
-    hea[7].setOrigin(606-489,631.5-510);
-    hea[i].scale(-0.5,-0.5);
+    hea[i].setTextureRect(IntRect(333,217.5,516-333,239.5));
+    // hea[i].rotate(180);
+    hea[7].setOrigin(396-333,101);
+    hea[i].scale(0.2,0.2);
     i++;
-    hea[i].setPosition(500-30,400-40);
+    hea[i].setPosition(500,400);
     hea[i].setTexture(heart);
-    hea[i].setTextureRect(IntRect(199.5,489.0,279,277));
-    hea[i].rotate(180);
-    hea[8].setOrigin(345-199.5,607.5-489);
-    hea[i].scale(-0.5,-0.5);
+    hea[i].setTextureRect(IntRect(516,217.5,763.5-516,239.5));
+    // hea[i].rotate(180);
+    hea[8].setOrigin(630-516,101);
+    hea[i].scale(0.2,0.2);
     i++;
-    hea[i].setPosition(500-30,400-40);
+    hea[i].setPosition(500,400);
     hea[i].setTexture(heart);
-    hea[i].setTextureRect(IntRect(0,0,348,351));
-    hea[i].rotate(180);
-    hea[9].setOrigin(177,151.5);
-    hea[i].scale(-0.5,-0.5);
+    hea[i].setTextureRect(IntRect(763.5,217.5,1014-763.5,239.5));
+    // hea[i].rotate(180);
+    hea[9].setOrigin(897-763.5,101);
+    hea[i].scale(0.2,0.2);
     // hea[4].setOrigin(937.5,627.0);
 
 
@@ -213,8 +258,8 @@ int main() {
             if(event.type == event.Closed) window1.close();
         }
         i++;
-        if(i==9)j++;
-        i%=10;
+        if(i==5-1)j++;
+        i%=5;
         j%=10;
         window1.clear();
         // for(j=0;j<10;j++)
