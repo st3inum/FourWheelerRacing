c3075b493e23765fa84ea0a18f2cbe56411a22a6
Tahseen Rasheed Chowdhury <35876394+TahseenSust@users.noreply.github.com>
2019-01-22 12:05:51 +0600

    Add files via upload

--- main/game.cpp
+++ main/game.cpp
@@ -282,7 +282,7 @@ class gari
     Sprite chaka1,chaka2,gari,pass_body,pass_matha,bg_w_1,bg_w_2,bg_w_3;
 
     int opacity=255;
-public:
+    public:
     int pos_x=-10,pos_y=386,pos_w_x=-10,d=1280000*2+15,wind_f=1280; // d for total distance you want to travel ;)
     void load_chaka(string txt)
     {
@@ -468,19 +468,183 @@ public:
     }
 };
 
+int game();
+
+class menu
+{
+    //game over
+    Texture game_over_t;
+    Sprite game_over_s;
+    int counter=0;
+
+
+    Texture menu_tex,menu_bar_w,menu_bar_b;
+    Sprite menu_sp,new_game,help,high_score,exit;
+
+    Text new_game_t,help_t,high_score_t,exit_t;
+    Font font;
+    public:
+    void init()
+    {
+        menu_tex.loadFromFile("file/bg_3.jpg");
+        menu_bar_b.loadFromFile("file/button_2.png");
+        menu_bar_w.loadFromFile("file/button_1.png");
+        menu_bar_w.setSmooth(true);
+        menu_bar_b.setSmooth(true);
+        menu_sp.setTexture(menu_tex);
+        new_game.setTexture(menu_bar_w);
+        help.setTexture(menu_bar_w);
+        high_score.setTexture(menu_bar_w);
+        exit.setTexture(menu_bar_w);
+
+        new_game.scale(0.5,0.5);
+        help.scale(0.5,0.5);
+        high_score.scale(0.5,0.5);
+        exit.scale(0.5,0.5);
+
+
+        new_game.setPosition(480,400-270);
+        help.setPosition(480,400-180);
+        high_score.setPosition(480,400-90);
+        exit.setPosition(480,400-0);
+
+
+        font.loadFromFile("file/font1.ttf");
+
+        new_game_t.setFont(font);
+        help_t.setFont(font);
+        high_score_t.setFont(font);
+        exit_t.setFont(font);
+
+        // menu_sp_t.setString("New Game");
+        new_game_t.setString("New Game");
+        help_t.setString("Help");
+        high_score_t.setString("HighScore");
+        exit_t.setString("Exit");
+
+
+        new_game_t.setFillColor(Color::White);
+        help_t.setFillColor(Color::White);
+        high_score_t.setFillColor(Color::White);
+        exit_t.setFillColor(Color::White);
+
+
+        new_game_t.setPosition(507,420-270);
+        help_t.setPosition(570,420-180);
+        high_score_t.setPosition(510,420-90);
+        exit_t.setPosition(570,420-0);
+
+
+
+        //game over
+
+
+        game_over_t.loadFromFile("file/game_over.png");
+        game_over_s.setTexture(game_over_t);
+        game_over_s.setPosition(400,300);
+
+    }
+    void draw()
+    {
+        if(counter<=0)
+        {
+            window1.draw(menu_sp);
+            window1.draw(new_game);
+            window1.draw(help);
+            window1.draw(high_score);
+            window1.draw(exit);
+            window1.draw(new_game_t);
+            window1.draw(help_t);
+            window1.draw(high_score_t);
+            window1.draw(exit_t);
+        }
+        else
+        {
+            window1.draw(game_over_s);
+        }
+    }
+    void move()
+    {
+        counter--;
+        if(counter<=0)
+        {
+            if(new_game.getGlobalBounds().contains(window1.mapPixelToCoords(Mouse::getPosition(window1))))
+            {
+                new_game_t.setFillColor(Color::Red);
+                new_game.setTexture(menu_bar_b);
+                if(Mouse::isButtonPressed(Mouse::Left))
+                    {
+                        if(game())counter=200;
+                        
+                    }
+            }
+            else
+            {
+                new_game.setTexture(menu_bar_w);
+                new_game_t.setFillColor(Color::White);
+            }
+            if(help.getGlobalBounds().contains(window1.mapPixelToCoords(Mouse::getPosition(window1))))
+            {
+                cout<<"help\n";
+                help_t.setFillColor(Color::Red);
+                help.setTexture(menu_bar_b);
+                if(Mouse::isButtonPressed(Mouse::Left));
+            }
+            else
+            {
+                help.setTexture(menu_bar_w);
+                help_t.setFillColor(Color::White);
+            }
+            if(high_score.getGlobalBounds().contains(window1.mapPixelToCoords(Mouse::getPosition(window1))))
+            {
+                cout<<"high_score\n";
+                high_score_t.setFillColor(Color::Red);
+                high_score.setTexture(menu_bar_b);
+                if(Mouse::isButtonPressed(Mouse::Left));
+            }
+            else
+            {
+                high_score.setTexture(menu_bar_w);
+                high_score_t.setFillColor(Color::White);
+            }
+            if(exit.getGlobalBounds().contains(window1.mapPixelToCoords(Mouse::getPosition(window1))))
+            {
+                // cout<<"click\n";
+                exit_t.setFillColor(Color::Red);
+                exit.setTexture(menu_bar_b);
+                if(Mouse::isButtonPressed(Mouse::Left))
+                    window1.close();
+            }
+            else
+            {
+                exit.setTexture(menu_bar_w);
+                exit_t.setFillColor(Color::White);
+            }
+        }
+    }
+};
+
+
+void main_menu();
+
 
 
 int main() {
-     //RenderWindow window1(VideoMode(win_W, 620), "Four Wheeler Racing");
-     //window1.setTitle("Four Wheeler Racing");
-     //window1.setSize(VideoMode(win_W, 620));
+    
+    main_menu();
+    return 0;
+}
+
+
+
+int game()
+{
+    
+
 
 
     // lifer kaj
-    Texture gameover_tex;
-    gameover_tex.loadFromFile("file/game_over.jpg");
-    Sprite gameover_sp;
-    gameover_sp.setTexture(gameover_tex);
+
     life ha;
     vector<life>ha_vec;
     int ha_timer=80;
@@ -586,235 +750,215 @@ int main() {
     new_gari.load_passenger_body("file/driver-body.png");
     new_gari.load_passenger_matha("file/passanger2-head.png");
     int fattor_blink_flag=0;
-
-    bool menu_b=1,alive=1;
-    Texture menu_tex;
-    menu_tex.loadFromFile("file/In_Game_Menu.jpg");
-    Sprite menu_sp;
-    menu_sp.setTexture(menu_tex);
-
-
     while(window1.isOpen()) {
         Event event;
         while(window1.pollEvent(event)) {
             if(event.type == event.Closed) window1.close();
         }
+
+        if(ha_timer>=300){
+            ha_timer=0;
+            ha.start_point=rand()%win_W;
+            ha.init();
+            ha_vec.push_back(ha);
+        }else{
+            ha_timer++;
+        }
+
+        if(timer_stone>120){//coin er timer_stone
+            int x=rand()%win_W;
+            tmp.load_points("file/stone.png",x);
+            stone_coin.push_back(tmp);
+            timer_stone=0;
+        }else{
+            timer_stone++;
+        }
+
+        if(timer_fattor>20){//fattor er timer_fattor
+            int x=rand()%win_W;
+            tmp_pat.load_points("file/fattor1.png",x);
+            pat.push_back(tmp_pat);
+            timer_fattor=0;
+        }else{
+            timer_fattor++;
+        }
+
         window1.clear(Color::White);
-        if(menu_b)
+        new_gari.move();
+        new_gari.print();
+
+        //stone - stone baad
+        for(int i=0;i<stone_coin.size();i++)
         {
-            window1.draw(menu_sp);
-            cout<<Mouse::getPosition(window1).x<<' '<<Mouse::getPosition(window1).y<<endl;
-            if(Mouse::isButtonPressed(Mouse::Left) && Mouse::getPosition(window1).x>=415 && Mouse::getPosition(window1).x<=609 && Mouse::getPosition(window1).y>=175 && Mouse::getPosition(window1).y<=208){
-                menu_b=0;
-                cout<<"click\n"<<endl;
+            for(int j=0;j<stone_coin.size();j++){
+                if(i!=j){
+                    if(stone_coin[i].pa.getGlobalBounds().intersects(stone_coin[j].pa.getGlobalBounds())){
+                        stone_coin.erase(stone_coin.begin()+j);
+                    }
+                }
             }
         }
 
-        else{
-            if(alive){
-                if(ha_timer>=300)
-                {
-                    ha_timer=0;
-                    ha.start_point=rand()%win_W;
-                    ha.init();
-                    ha_vec.push_back(ha);
-                }
-                else
-                {
-                    ha_timer++;
-                }
 
-                if(timer_stone>120) //coin er timer_stone
-                {
-                    int x=rand()%win_W;
-                    tmp.load_points("file/stone.png",x);
-                    stone_coin.push_back(tmp);
-                    timer_stone=0;
-                }
-                else
-                {
-                    timer_stone++;
-                }
-
-                if(timer_fattor>200) //fattor er timer_fattor
-                {
-                    int x=rand()%win_W;
-                    tmp_pat.load_points("file/fattor1.png",x);
-                    pat.push_back(tmp_pat);
-                    timer_fattor=0;
-                }
-                else
-                {
-                    timer_fattor++;
-                }
-
-
-                new_gari.move();
-                new_gari.print();
-
-                //stone - stone baad
-                for(int i=0; i<stone_coin.size(); i++)
-                {
-                    for(int j=0; j<stone_coin.size(); j++)
-                    {
-                        if(i!=j)
-                        {
-                            if(stone_coin[i].pa.getGlobalBounds().intersects(stone_coin[j].pa.getGlobalBounds()))
-                            {
-                                stone_coin.erase(stone_coin.begin()+j);
-                            }
-                        }
+        // fattor - fattor baad
+        for(int i=0;i<pat.size();i++)
+        {
+            for(int j=0;j<pat.size();j++){
+                if(i!=j){
+                    if(pat[i].pa.getGlobalBounds().intersects(pat[j].pa.getGlobalBounds())){
+                        pat.erase(pat.begin()+j);
                     }
                 }
-
-
-                // fattor - fattor baad
-                for(int i=0; i<pat.size(); i++)
-                {
-                    for(int j=0; j<pat.size(); j++)
-                    {
-                        if(i!=j)
-                        {
-                            if(pat[i].pa.getGlobalBounds().intersects(pat[j].pa.getGlobalBounds()))
-                            {
-                                pat.erase(pat.begin()+j);
-                            }
-                        }
-                    }
-                }
-
-                // stone - fattor baad
-                for(int i=0; i<stone_coin.size(); i++)
-                {
-                    for(int j=0; j<pat.size(); j++)
-                    {
-                        if(stone_coin[i].pa.getGlobalBounds().intersects(pat[j].pa.getGlobalBounds()))
-                        {
-                            pat.erase(pat.begin()+j);
-                        }
-                    }
-                }
-
-
-                // point count
-                for(int i=0; i<stone_coin.size(); i++)
-                {
-                    if(new_gari.collusion(stone_coin[i].pa))
-                    {
-                        point++;
-                        stone_coin.erase(stone_coin.begin()+i);
-                        text_points.setString(to_string(point));
-                    }
-                }
-                // life increase count
-                for(int i=0; i<ha_vec.size(); i++)
-                {
-                    for(int j=0; j<10; j++)
-                        if(new_gari.collusion(ha_vec[i].hea[j]))
-                        {
-                            health=min(health+10.0,275.0);
-                            ha_vec.erase(ha_vec.begin()+i);
-
-                            text_health.setString(to_string((int)(health/2.75))+" %");
-                            break;
-                        }
-                }
-
-                // health reduce
-
-                for(int i=0; i<pat.size(); i++)
-                {
-                    if(new_gari.matha_collusion(pat[i].pa))
-                    {
-                        health/=2;
-                        pat.erase(pat.begin()+i);
-                        fattor_blink_flag=40;
-                        //text_health.setString(to_string((int)(health/2.75))+" %");
-                    }
-                    else if(new_gari.collusion(pat[i].pa))
-                    {
-                        health-=10;
-                        pat.erase(pat.begin()+i);
-                        fattor_blink_flag=40;
-                    }
-                    if(health<=0)
-                    {
-                        alive=0;
-                        break;
-                    }
-                    text_health.setString(to_string((int)(health/2.75))+" %");
-                }
-                if(fattor_blink_flag>0)
-                {
-                    new_gari.blink();
-                    fattor_blink_flag--;
-                }
-                else
-                {
-                    new_gari.thik_hoye_ja();
-                }
-
-                // stone move + erase
-                for(int i=0; i<stone_coin.size(); i++)
-                {
-                    stone_coin[i].move();
-                    if(stone_coin[i].f)
-                    {
-                        stone_coin[i].blink();
-                        if(stone_coin[i].op_cnt>=80)
-                            stone_coin.erase(stone_coin.begin()+i);
-                    }
-                }
-
-
-                for(int i=0; i<stone_coin.size(); i++)
-                {
-                    stone_coin[i].print();
-                }
-
-
-                //fattor move + erase
-                for(int i=0; i<pat.size(); i++)
-                {
-                    pat[i].move();
-                    if(pat[i].f)
-                    {
-                        pat[i].blink();
-                        if(pat[i].op_cnt>80)
-                            pat.erase(pat.begin()+i);
-                    }
-                }
-
-
-                for(int i=0; i<pat.size(); i++)
-                {
-                    pat[i].print();
-                }
-                coin_image_number++;
-                coin_image_number%=10*framerate;
-                window1.draw(coin[coin_image_number/framerate]);
-
-                for(int i=0; i<ha_vec.size(); i++)
-                {
-                    ha_vec[i].move();
-                    ha_vec[i].draw();
-                }
-
-
-
-                healthbar.setSize(Vector2f(health,50));
-                window1.draw(healthbar);
-                window1.draw(HealthBar);
-                window1.draw(text_health);
-                window1.draw(text_points);
-
-            }else{
-                window1.draw(gameover_sp);
             }
-
         }
+
+        // stone - fattor baad
+        for(int i=0;i<stone_coin.size();i++)
+        {
+            for(int j=0;j<pat.size();j++){
+                if(stone_coin[i].pa.getGlobalBounds().intersects(pat[j].pa.getGlobalBounds())){
+                    pat.erase(pat.begin()+j);
+                }
+            }
+        }
+
+
+        // point count
+        for(int i=0;i<stone_coin.size();i++)
+        {
+            if(new_gari.collusion(stone_coin[i].pa))
+            {
+                point++;
+                stone_coin.erase(stone_coin.begin()+i);
+                text_points.setString(to_string(point));
+            }
+        }
+        // life increase count
+        for(int i=0;i<ha_vec.size();i++)
+        {
+            for(int j=0;j<10;j++)
+            if(new_gari.collusion(ha_vec[i].hea[j]))
+            {
+                health=min(health+10.0,275.0);
+                ha_vec.erase(ha_vec.begin()+i);
+
+                text_health.setString(to_string((int)(health/2.75))+" %");
+                break;
+            }
+        }
+
+        // health reduce
+
+        for(int i=0;i<pat.size();i++)
+        {
+            if(new_gari.matha_collusion(pat[i].pa))
+            {
+                health/=2;
+                pat.erase(pat.begin()+i);
+                fattor_blink_flag=40;
+                //text_health.setString(to_string((int)(health/2.75))+" %");
+            }
+            else if(new_gari.collusion(pat[i].pa))
+            {
+                health-=10;
+                pat.erase(pat.begin()+i);
+                fattor_blink_flag=40;
+            }
+            if(health<=0)
+            {
+                return 1;
+            }
+            text_health.setString(to_string((int)(health/2.75))+" %");
+        }
+        if(fattor_blink_flag>0)
+        {
+            new_gari.blink();
+            fattor_blink_flag--;
+        }
+        else
+        {
+            new_gari.thik_hoye_ja();
+        }
+
+        // stone move + erase
+        for(int i=0;i<stone_coin.size();i++){
+            stone_coin[i].move();
+            if(stone_coin[i].f){
+                stone_coin[i].blink();
+                if(stone_coin[i].op_cnt>=80)
+                    stone_coin.erase(stone_coin.begin()+i);
+            }
+        }
+
+
+        for(int i=0;i<stone_coin.size();i++){
+            stone_coin[i].print();
+        }
+
+
+        //fattor move + erase
+        for(int i=0;i<pat.size();i++){
+            pat[i].move();
+            if(pat[i].f){
+                pat[i].blink();
+                if(pat[i].op_cnt>80)
+                    pat.erase(pat.begin()+i);
+            }
+        }
+
+
+        for(int i=0;i<pat.size();i++){
+            pat[i].print();
+        }
+        coin_image_number++;
+        coin_image_number%=10*framerate;
+        window1.draw(coin[coin_image_number/framerate]);
+
+        for(int i=0;i<ha_vec.size();i++){
+            ha_vec[i].move();
+            ha_vec[i].draw();
+        }
+
+
+
+        healthbar.setSize(Vector2f(health,50));
+        window1.draw(healthbar);
+        window1.draw(HealthBar);
+        window1.draw(text_health);
+        window1.draw(text_points);
         // cout<< "# "<<health << ' '<<point<<endl;
         window1.display();
     }
-    return 0;
+}
+
+
+void main_menu()
+{
+    // game();
+    
+    menu new_menu;
+    new_menu.init();
+    while(window1.isOpen()) {
+        Event event;
+            while(window1.pollEvent(event)) {
+                if(event.type == event.Closed) window1.close();
+            }
+
+        window1.clear();
+        new_menu.move();
+        new_menu.draw();
+
+
+       // cout<<Mouse::getPosition(window1).x<<' '<<Mouse::getPosition(window1).y<<endl;
+       // if(Mouse::isButtonPressed(Mouse::Left) && Mouse::getPosition().x>=988 && Mouse::getPosition().x<=1182 && Mouse::getPosition().y>=414 && Mouse::getPosition().y<=448){
+       //     cout<<"click\n";
+       // }
+
+
+     window1.display();
+    // Mouse is inside the sprite.
+
+
+
+ }
 }
--- main/menu.cpp
+++ main/menu.cpp
@@ -1,35 +1,113 @@
-//#include<SFML/Graphics.hpp>
-//using namespace sf;
-//#include <bits/stdc++.h>
-//using namespace std;
-//#define win_W 1280
-//// RenderWindow window1;
-//RenderWindow window1(VideoMode(win_W, 620), "Four Wheeler Racing");
-//
-//
-//
-//int main() {
-//    window1.setFramerateLimit(60);
-//    Texture menu_tex;
-//    menu_tex.loadFromFile("file/In_Game_Menu.jpg");
-//    Sprite menu_sp;
-//    menu_sp.setTexture(menu_tex);
-//    while(window1.isOpen()) {
-//        Event event;
-//        while(window1.pollEvent(event)) {
-//            if(event.type == event.Closed) window1.close();
-//        }
-//
-//        window1.clear();
-//
-//        window1.draw(menu_sp);
-//        //cout<<Mouse::getPosition().x<<' '<<Mouse::getPosition().y<<endl;
-//        if(Mouse::isButtonPressed(Mouse::Left) && Mouse::getPosition().x>=988 && Mouse::getPosition().x<=1182 && Mouse::getPosition().y>=414 && Mouse::getPosition().y<=448){
-//            cout<<"click\n";
-//        }
-//        window1.display();
-//
-//
-//    }
-//
-//}
+#include<SFML/Graphics.hpp>
+using namespace sf;
+#include <bits/stdc++.h>
+using namespace std;
+#define win_W 1280
+// RenderWindow window1;
+RenderWindow window1(VideoMode(win_W, 620), "Four Wheeler Racing");
+
+class menu
+{
+	Texture menu_tex,menu_bar_w,menu_bar_b;
+	Sprite menu_sp,new_game,help,high_score,exit;
+	public:
+	void init()
+	{
+		menu_tex.loadFromFile("file/bg_3.jpg");
+		menu_bar_b.loadFromFile("file/button_2.png");
+		menu_bar_w.loadFromFile("file/button_1.png");
+		menu_bar_w.setSmooth(true);
+		menu_bar_b.setSmooth(true);
+		menu_sp.setTexture(menu_tex);
+		new_game.setTexture(menu_bar_w);
+		help.setTexture(menu_bar_w);
+		high_score.setTexture(menu_bar_w);
+		exit.setTexture(menu_bar_w);
+
+		new_game.scale(0.5,0.5);
+		help.scale(0.5,0.5);
+		high_score.scale(0.5,0.5);
+		exit.scale(0.5,0.5);
+
+
+		new_game.setPosition(480,400-270);
+		help.setPosition(480,400-180);
+		high_score.setPosition(480,400-90);
+		exit.setPosition(480,400-0);
+	}
+	void draw()
+	{
+		window1.draw(menu_sp);
+		window1.draw(new_game);
+		window1.draw(help);
+		window1.draw(high_score);
+		window1.draw(exit);
+	}
+	void move(bool f)
+	{
+		if(Mouse::isButtonPressed(Mouse::Left) && new_game.getGlobalBounds().contains(window1.mapPixelToCoords(Mouse::getPosition(window1))))
+		{
+			cout<<"click\n";
+			new_game.setTexture(menu_bar_b);
+		}
+		if(Mouse::isButtonPressed(Mouse::Left) && help.getGlobalBounds().contains(window1.mapPixelToCoords(Mouse::getPosition(window1))))
+		{
+			cout<<"help\n";
+			help.setTexture(menu_bar_b);
+		}
+		if(Mouse::isButtonPressed(Mouse::Left) && high_score.getGlobalBounds().contains(window1.mapPixelToCoords(Mouse::getPosition(window1))))
+		{
+			cout<<"high_score\n";
+			high_score.setTexture(menu_bar_b);
+		}
+		if(Mouse::isButtonPressed(Mouse::Left) && exit.getGlobalBounds().contains(window1.mapPixelToCoords(Mouse::getPosition(window1))))
+		{
+			// cout<<"click\n";
+			window1.close();
+			exit.setTexture(menu_bar_b);
+		}
+		if(f)
+		{
+			new_game.setTexture(menu_bar_w);
+			help.setTexture(menu_bar_w);
+			high_score.setTexture(menu_bar_w);
+			exit.setTexture(menu_bar_w);
+		}
+
+	}
+};
+
+
+
+
+int main() {
+   window1.setFramerateLimit(60);
+   menu new_menu;
+   new_menu.init();
+   while(window1.isOpen()) {
+       Event event;
+       while(window1.pollEvent(event)) {
+           if(event.type == event.Closed) window1.close();
+       }
+
+       window1.clear();
+       new_menu.move(0);
+       new_menu.draw();
+
+       
+       // cout<<Mouse::getPosition(window1).x<<' '<<Mouse::getPosition(window1).y<<endl;
+       // if(Mouse::isButtonPressed(Mouse::Left) && Mouse::getPosition().x>=988 && Mouse::getPosition().x<=1182 && Mouse::getPosition().y>=414 && Mouse::getPosition().y<=448){
+       //     cout<<"click\n";
+       // }
+		if(event.type == event.MouseButtonReleased && event.mouseButton.button== sf::Mouse::Left)new_menu.move(1);
+
+		
+		
+       window1.display();
+    // Mouse is inside the sprite.
+ 		
+
+
+   }
+
+}
